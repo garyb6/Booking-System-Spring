89a9730334d6db8ae7ba28887625fb6925ae8dca
garyb6 <gary.bennett6@outlook.com>
2022-02-24 10:21:19 +0000

    course model

--- src/main/java/com/courseBookingSystem/courseBookingSystem/models/Course.java
+++ src/main/java/com/courseBookingSystem/courseBookingSystem/models/Course.java
@@ -1,4 +1,66 @@
 package com.courseBookingSystem.courseBookingSystem.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Course {
+
+    private Long id;
+
+    private String name;
+
+    private String town;
+
+    private int rating;
+
+    private List<Booking> bookings;
+
+    public Course(String name, String town, int rating) {
+        this.name = name;
+        this.town = town;
+        this.rating = rating;
+        this.bookings = new ArrayList<>();
+    }
+
+    public Course(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
 }
