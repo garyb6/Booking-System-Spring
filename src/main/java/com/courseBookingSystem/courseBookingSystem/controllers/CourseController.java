3612c5324beccb541c9586828aa0d27a72770a67
garyb6 <gary.bennett6@outlook.com>
2022-02-24 11:21:09 +0000

    get by rating now works

--- src/main/java/com/courseBookingSystem/courseBookingSystem/controllers/CourseController.java
+++ src/main/java/com/courseBookingSystem/courseBookingSystem/controllers/CourseController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,7 +19,16 @@ public class CourseController {
     CourseRepository courseRepository;
 
     @GetMapping(value = "/courses")
-    public ResponseEntity<List<Course>> getAllCourses(){
+    public ResponseEntity<List<Course>> getAllCourses(
+            @RequestParam(required = false, name = "rating") Integer rating,
+            @RequestParam(required = false, name = "customerName") String  name
+    ){
+        if(rating != null){
+            return new ResponseEntity<>(courseRepository.findCoursesByRating(rating), HttpStatus.OK);
+        }
+        if(name != null){
+            return new ResponseEntity<>(courseRepository.findCoursesByBookingsCustomerName(name), HttpStatus.OK);
+        }
         return new ResponseEntity<>(courseRepository.findAll(), HttpStatus.OK);
     }
 
--- src/main/java/com/courseBookingSystem/courseBookingSystem/repositories/CourseRepository.java
+++ src/main/java/com/courseBookingSystem/courseBookingSystem/repositories/CourseRepository.java
@@ -5,6 +5,11 @@ import com.courseBookingSystem.courseBookingSystem.models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findCoursesByRating(int rating);
+    List<Course> findCoursesByBookingsCustomerName(String name);
 }
